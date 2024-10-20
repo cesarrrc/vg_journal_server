caa0ed7dbe3c6d016a2af881c38cdd9abdfaa420
Sam Dunegan <cesarcisneros9@gmail.com>
2024-10-19 21:05:37 -0500

    switched to conneciton pool class

--- utils/connection.js
+++ utils/connection.js
@@ -1,11 +1,25 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.HOST_NAME,
-  user: process.env.DATABASE_USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
-// exporting for the index file
-module.exports = connection;
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log("creating connection pool...");
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: process.env.HOST_NAME,
+        user: process.env.DATABASE_USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+      });
+
+      return this.pool;
+    }
+
+    return this.pool;
+  }
+}
+
+const instance = new Connection();
+
+module.exports = instance;
